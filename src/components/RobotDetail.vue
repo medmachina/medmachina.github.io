4f04fe33990b73645aed2d2d83ab15cfb6777da2
Anton Deguet <anton.deguet@jhu.edu>
2025-11-21 02:53:42 -0500

    Testing edit link

--- src/components/RobotDetail.vue
+++ src/components/RobotDetail.vue
@@ -6,24 +6,9 @@
         {{ filteredProject["name"] }}
         <span v-if="companyInfo" style="font-size:.85em;">(<router-link :to="`/company/${companyInfo.name}`">{{ companyInfo.name }}</router-link>)</span>
       </h1>
-      <div style="display: flex; align-items: center; gap: 1rem;">
-        <a 
-          v-if="project"
-          :href="`https://github.com/medmachina/medmachina.github.io/edit/main/public/robots.json`" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          class="btn btn-sm btn-outline-secondary"
-          title="Edit this robot on GitHub"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" style="margin-right: 4px;">
-            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-          </svg>
-          Edit on GitHub
-        </a>
-        <router-link to="/" style="display: flex; align-items: center; text-decoration: none;">
-          <img src="/text-logo.svg" alt="medmachina" style="height:41px; width:auto;" />
-        </router-link>
-      </div>
+      <router-link to="/" style="display: flex; align-items: center; text-decoration: none; margin-left: 1rem;">
+        <img src="/text-logo.svg" alt="medmachina" style="height:41px; width:auto;" />
+      </router-link>
     </div>
 
     <div class="project-description">
@@ -81,6 +66,22 @@
           </div>
         </div>
       </div>
+    </div>
+
+    <!-- Edit on GitHub Section -->
+    <div v-if="project" class="edit-section mt-4">
+      <a 
+        :href="`https://github.com/medmachina/medmachina.github.io/edit/main/public/robots.json`" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        class="btn btn-outline-secondary"
+        title="Edit this robot on GitHub"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" style="margin-right: 4px;">
+          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+        </svg>
+
+      </a>
     </div>
 
     <div v-if="!project">
