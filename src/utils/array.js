91b363eaa3e8fe1563aac03c08deb7a60d147116
Anton Deguet <anton.deguet@jhu.edu>
2025-11-21 03:46:44 -0500

    wip

---
+++ src/utils/array.js
@@ -0,0 +1,13 @@
+/**
+ * Shuffle an array using the Fisher-Yates algorithm
+ * @param {Array} array - The array to shuffle
+ * @returns {Array} A new shuffled array
+ */
+export function shuffleArray(array) {
+  const newArray = [...array]
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]]
+  }
+  return newArray
+}
